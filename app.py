6cc046de7928f4e0a2e698dacf212ca9d7ae4e08
Pushpendra verma <pushpendra.easy@gmail.com>
2024-02-24 23:26:23 +0800

    update port

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def clear_messages():
     return jsonify({'message': 'All messages cleared successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8081, debug=True)
